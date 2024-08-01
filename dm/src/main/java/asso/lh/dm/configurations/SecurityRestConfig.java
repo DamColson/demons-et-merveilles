3c012c60e03bbdf70c7b71fd03febc537c0c8af2
DamColson <Damien.colson@gmail.com>
2024-08-01 16:48:55 +0200

    securité basique temporelle, basic auth permit all

---
+++ dm/src/main/java/asso/lh/dm/configurations/SecurityRestConfig.java
@@ -0,0 +1,33 @@
+package asso.lh.dm.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityRestConfig {
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		http.csrf(csrf->csrf.disable());
+		http.authorizeHttpRequests(auth->{
+			auth.requestMatchers(HttpMethod.OPTIONS).permitAll()
+			.anyRequest().permitAll();
+		});
+		
+		http.sessionManagement(manager->manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		http.httpBasic(Customizer.withDefaults());
+		return http.build();
+	}
+}
