e9cf5a6b624fc37efb686611ef70c41ed1ae9154
DamColson <Damien.colson@gmail.com>
2024-08-02 17:18:23 +0200

    Creation des services et ajouts des methodes basique, delete a faire

---
+++ dm/src/main/java/asso/lh/dm/services/AdmGameService.java
@@ -0,0 +1,63 @@
+package asso.lh.dm.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import asso.lh.dm.dao.IDAOAdmGame;
+import asso.lh.dm.model.AdmGame;
+import asso.lh.dm.model.Theme;
+
+@Service
+public class AdmGameService {
+	
+	@Autowired
+	private IDAOAdmGame daoAdmGame;
+	
+	public List<AdmGame> getAll(){
+		return daoAdmGame.findAll();
+	}
+	
+	public AdmGame getById(Integer id) {
+		if(id==null) {
+			throw new RuntimeException("id null");
+		}
+		return daoAdmGame.findById(id).orElseThrow(()->new RuntimeException("Aucun jeu n'existe pour cet id"));
+	}
+	
+	public AdmGame getByName(String name) {
+		if(name==null) {
+			throw new RuntimeException("nom null");
+		}
+		return daoAdmGame.findByName(name).orElseThrow(()->new RuntimeException("Aucun jeu n'existe pour ce nom"));
+	}
+	
+	public List<AdmGame> getByTheme(Theme theme) {
+		if(theme==null) {
+			throw new RuntimeException("theme null");
+		}
+		return daoAdmGame.findByTheme(theme);
+	}
+	
+	public AdmGame insert(AdmGame game) {
+		if(game==null) {
+			throw new RuntimeException("jeu null");
+		}
+		return daoAdmGame.save(game);
+	}
+	
+	public AdmGame update(AdmGame game) {
+		if(game==null) {
+			throw new RuntimeException("jeu null");
+		}
+		if(game.getId()==null) {
+			throw new RuntimeException("Aucun jeu n'existe pour cet id");
+		}
+		return daoAdmGame.save(game);
+	}
+	
+	public void delete(AdmGame game) {
+		
+	}
+}
---
+++ dm/src/main/java/asso/lh/dm/services/AdmTableService.java
@@ -0,0 +1,72 @@
+package asso.lh.dm.services;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import asso.lh.dm.dao.IDAOAdmTable;
+import asso.lh.dm.model.AdmGame;
+import asso.lh.dm.model.AdmMember;
+import asso.lh.dm.model.AdmTable;
+
+@Service
+public class AdmTableService {
+
+	@Autowired
+	private IDAOAdmTable daoAdmTable;
+	
+	public List<AdmTable> getAll(){
+		return daoAdmTable.findAll();
+	}
+	
+	public AdmTable getById(Integer id) {
+		if(id == null) {
+			throw new RuntimeException("id null");
+		}
+		return daoAdmTable.findById(id).orElseThrow(()->new RuntimeException("Aucune table n'existe pour cet id"));
+	}
+	
+	public List<AdmTable> getByDate(LocalDate date){
+		if(date == null) {
+			throw new RuntimeException("date null");
+		}
+		return daoAdmTable.findByDate(date);
+	}
+	
+	public List<AdmTable> getByGame(AdmGame game){
+		if(game == null) {
+			throw new RuntimeException("game null");
+		}
+		return daoAdmTable.findByGame(game);
+	}
+	
+	public List<AdmTable> getByGameMaster(AdmMember gameMaster) {
+		if(gameMaster==null) {
+			throw new RuntimeException("game master null");
+		}
+		return daoAdmTable.findByGameMaster(gameMaster);
+	}
+	
+	public AdmTable insert(AdmTable table) {
+		if(table == null) {
+			throw new RuntimeException("table null");
+		}
+		return daoAdmTable.save(table);
+	}
+	
+	public AdmTable update(AdmTable table) {
+		if(table == null) {
+			throw new RuntimeException("table null");
+		}
+		if(table.getId()==null) {
+			throw new RuntimeException("Aucune table n'existe pour cet id");
+		}
+		return daoAdmTable.save(table);
+	}
+	
+	public void delete(AdmTable table) {
+		
+	}
+}
